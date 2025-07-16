62bb78cbf1231d5f5a8276cca7f8524e42cc28de
Rameshraj55 <mrrameshpvm@gmail.com>
2025-07-16 16:56:08 +0530

    added fraud rule evaluation function for interface

--- src/main/java/com/payshield/rules/engine/FraudRule.java
+++ src/main/java/com/payshield/rules/engine/FraudRule.java
@@ -1,4 +1,8 @@
 package com.payshield.rules.engine;
 
+import com.payshield.model.entity.Transaction;
+
 public interface FraudRule {
+    boolean isFraudulent(Transaction transaction);
+    String getReason();  // Reason for alert if fraud detected
 }
