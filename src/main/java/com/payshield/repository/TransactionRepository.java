826d019545c425d209eb198e7c2bd026d4028cc1
Rameshraj55 <mrrameshpvm@gmail.com>
2025-07-17 12:12:30 +0530

    added countByUserIdAndTimestampBetween for TransactionRepo

--- src/main/java/com/payshield/repository/TransactionRepository.java
+++ src/main/java/com/payshield/repository/TransactionRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    int countByUserIdAndTimestampBetween(Long userId, Long from, Long to);
+
+
 }
