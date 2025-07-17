78f873a5284771eea92ba913780b292cdfbfaf11
Rameshraj55 <mrrameshpvm@gmail.com>
2025-07-17 12:09:13 +0530

    added fraud transaction detection logic in rules implementation module

--- src/main/java/com/payshield/rules/impl/AmountThresholdRule.java
+++ src/main/java/com/payshield/rules/impl/AmountThresholdRule.java
@@ -1,4 +1,21 @@
 package com.payshield.rules.impl;
 
-public class AmountThresholdRule {
+import com.payshield.model.entity.Transaction;
+import com.payshield.rules.engine.FraudRule;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AmountThresholdRule implements FraudRule {
+
+    private static final double MAX_ALLOWED_AMOUNT = 100000.00;
+
+    @Override
+    public boolean isFraudulent(Transaction tx) {
+        return tx.getAmount() > MAX_ALLOWED_AMOUNT;
+    }
+
+    @Override
+    public String getReason() {
+        return "Transaction amount exceeds the permitted threshold.";
+    }
 }
--- src/main/java/com/payshield/rules/impl/VelocityRule.java
+++ src/main/java/com/payshield/rules/impl/VelocityRule.java
@@ -1,4 +1,31 @@
 package com.payshield.rules.impl;
 
-public class VelocityRule {
+import com.payshield.model.entity.Transaction;
+import com.payshield.repository.TransactionRepository;
+import com.payshield.rules.engine.FraudRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VelocityRule implements FraudRule {
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    private static final long TIME_WINDOW_MILLIS = 60 * 1000; // 1 minute
+    private static final int MAX_TX_PER_MINUTE = 3;
+
+    @Override
+    public boolean isFraudulent(Transaction tx) {
+        long currentTime = System.currentTimeMillis();
+        long windowStart = currentTime - TIME_WINDOW_MILLIS;
+        int count = transactionRepository.countByUserIdAndTimestampBetween(
+                tx.getuserId(), windowStart, currentTime
+        );
+        return count >= MAX_TX_PER_MINUTE;
+    }
+
+    @Override
+    public String getReason() {
+        return "High frequency of transactions in a short time.";
+    }
 }
