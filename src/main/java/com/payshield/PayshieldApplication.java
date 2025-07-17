712b32732323c8d45ce6fb8836cae06d83fa6337
Rameshraj55 <mrrameshpvm@gmail.com>
2025-07-17 12:10:21 +0530

    added main annotation for Spring boot main application

--- src/main/java/com/payshield/PayshieldApplication.java
+++ src/main/java/com/payshield/PayshieldApplication.java
@@ -1,5 +1,12 @@
 package com.payshield;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.payshield") // Ensures all your packages are scanned
+@EnableJpaRepositories(basePackages = "com.payshield.repository") // Enable JPA Repos
 public class PayshieldApplication {
     public static void main(String[] args) {
 
