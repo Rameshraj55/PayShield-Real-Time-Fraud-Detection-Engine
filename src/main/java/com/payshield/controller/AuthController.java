77d7cefb84c759fdea77b9d111390050b1134521
Rameshraj55 <mrrameshpvm@gmail.com>
2025-07-21 18:13:57 +0530

    implemented REST API end-points

--- src/main/java/com/payshield/controller/AuthController.java
+++ src/main/java/com/payshield/controller/AuthController.java
@@ -1,4 +1,37 @@
 package com.payshield.controller;
 
+
+import com.payshield.model.dto.AuthRequest;
+import com.payshield.model.dto.AuthResponse;
+import com.payshield.model.dto.RegisterRequest;
+import com.payshield.service.JwtService;
+import com.payshield.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        userService.registerUser(request);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        String token = userService.authenticateAndGenerateToken(request);
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+
 }
--- src/main/java/com/payshield/controller/TransactionController.java
+++ src/main/java/com/payshield/controller/TransactionController.java
@@ -1,4 +1,32 @@
 package com.payshield.controller;
 
+import com.payshield.model.dto.TransactionRequest;
+import com.payshield.model.entity.Transaction;
+import com.payshield.service.FraudDetectionService;
+import com.payshield.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private FraudDetectionService fraudDetectionService;
+
+    @PostMapping
+    public ResponseEntity<?> processTransaction(@RequestBody TransactionRequest request) {
+        Transaction savedTx = transactionService.saveTransaction(request);
+        boolean isFraud = fraudDetectionService.evaluateTransaction(savedTx);
+        return ResponseEntity.ok("Transaction processed. Fraud status: " + isFraud);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable Long id) {
+        return ResponseEntity.ok(transactionService.getTransactionById(id));
+    }
+
 }
